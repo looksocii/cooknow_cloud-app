f9ea29637aa70b10815064f1749758ded6b05620
Puntakarn Kaewsanghan <aopup1122@gmail.com>
2022-02-27 22:32:55 +0700

    Update Backend

--- NodeJS-Backend-App/index.js
+++ NodeJS-Backend-App/index.js
@@ -6,6 +6,7 @@ const swaggerDocument = require("./swagger.json")
 const express = require("express")
 const bodyParser = require("body-parser")
 
+// Import SQL Query and Create App
 const db = require("./queries")
 const app = express()
 const port = 3000
@@ -23,15 +24,29 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Create APIs
 
-// User Account
+// ----------------- User Account -----------------
 app.get("/users", db.getUsers)
 app.post("/user", db.createUser)
 app.put("/user/:id", db.updateUser)
 app.delete("/deleteuser/:id", db.deleteUser)
+// ------------------------------------------------
 
-// Food Menu
+// ------------------ Food Menu ------------------
 app.get("/foods", db.getFoods)
 app.post("/food", db.createFood)
+app.put("/food/:id", db.updateFood)
+app.delete("/deletefood/:id", db.deleteFood)
+// -----------------------------------------------
+
+// ----------------- Review Food -----------------
+app.post("/review", db.createReview)
+app.get("/reviews/:id", db.getReviews)
+// -----------------------------------------------
+
+// ----------------- Share Food -----------------
+app.post("/share", db.createShare)
+app.get("/sharese/:id", db.getSharese)
+// ----------------------------------------------
 
 // Listen to Port 3000
 app.listen(port, () => {
--- NodeJS-Backend-App/queries.js
+++ NodeJS-Backend-App/queries.js
@@ -49,11 +49,7 @@ const updateUser = (request, response) => {
             } else {
                 // Response and Logging
                 response.status(200).send(`User modified with ID : ${id}`)
-                logger.info(
-                    "PUT /" +
-                        request.get("host") +
-                        request.originalUrl
-                )
+                logger.info("PUT /" + request.get("host") + request.originalUrl)
             }
         }
     )
@@ -74,9 +70,7 @@ const createUser = (request, response) => {
                     .status(201)
                     .send(`User added with User Name : ${UserName}`)
                 logger.info(
-                    "POST /" +
-                        request.get("host") +
-                        request.originalUrl
+                    "POST /" + request.get("host") + request.originalUrl
                 )
             }
         }
@@ -86,19 +80,21 @@ const createUser = (request, response) => {
 // Delete User Account
 const deleteUser = (request, response) => {
     const id = parseInt(request.params.id)
-    pool.query("DELETE FROM UserAccount WHERE id = $1", [id], (error, results) => {
-        if (error) {
-            throw error
-        } else {
-            // Response and Logging
-            response.status(200).send(`User deleted with ID : ${id}`)
-            logger.info(
-                "DELETE /" +
-                    request.get("host") +
-                    request.originalUrl
-            )
+    pool.query(
+        "DELETE FROM UserAccount WHERE id = $1",
+        [id],
+        (error, results) => {
+            if (error) {
+                throw error
+            } else {
+                // Response and Logging
+                response.status(200).send(`User deleted with ID : ${id}`)
+                logger.info(
+                    "DELETE /" + request.get("host") + request.originalUrl
+                )
+            }
         }
-    })
+    )
 }
 
 // -----------------------------------------------------------------------------
@@ -118,6 +114,25 @@ const getFoods = (request, response) => {
     })
 }
 
+// Update Food Menu
+const updateFood = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { FoodName, FoodDetail, AddDate, UserAccountID } = request.body
+    pool.query(
+        "UPDATE FoodMenu SET Food_Name = $1, Food_Detail = $2, Add_Date = $3, UserAccount_ID = $4 WHERE id = $5",
+        [FoodName, FoodDetail, AddDate, UserAccountID, id],
+        (error, results) => {
+            if (error) {
+                throw error
+            } else {
+                // Response and Logging
+                response.status(200).send(`Food modified with ID : ${id}`)
+                logger.info("PUT /" + request.get("host") + request.originalUrl)
+            }
+        }
+    )
+}
+
 // Create Food Menu
 const createFood = (request, response) => {
     const { FoodName, FoodDetail, AddDate, UserAccountID } = request.body
@@ -133,10 +148,111 @@ const createFood = (request, response) => {
                     .status(201)
                     .send(`Food added with Food Name : ${FoodName}`)
                 logger.info(
-                    "POST /" +
-                        request.get("host") +
-                        request.originalUrl
+                    "POST /" + request.get("host") + request.originalUrl
                 )
+            }
+        }
+    )
+}
+
+// Delete Food Menu
+const deleteFood = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query("DELETE FROM FoodMenu WHERE id = $1", [id], (error, results) => {
+        if (error) {
+            throw error
+        } else {
+            // Response and Logging
+            response.status(200).send(`Food deleted with ID : ${id}`)
+            logger.info("DELETE /" + request.get("host") + request.originalUrl)
+        }
+    })
+}
+
+// -----------------------------------------------------------------------------
+//                      Review Food Table
+// -----------------------------------------------------------------------------
+
+// Create Review Food
+const createReview = (request, response) => {
+    const { ReviewDetail, Rating, ReviewDate, UserAccountID, FoodMenuID } =
+        request.body
+    pool.query(
+        "INSERT INTO ReviewFood (Review_Detail, Rating, Review_Date, UserAccount_ID, FoodMenu_ID) VALUES ($1, $2, $3, $4, $5)",
+        [ReviewDetail, Rating, ReviewDate, UserAccountID, FoodMenuID],
+        (error, results) => {
+            if (error) {
+                logger.error(error)
+            } else {
+                // Response and Logging
+                response
+                    .status(201)
+                    .send(`Review added to food ID : ${FoodMenuID}`)
+                logger.info(
+                    "POST /" + request.get("host") + request.originalUrl
+                )
+            }
+        }
+    )
+}
+
+// View All Review of Food
+const getReviews = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query(
+        "SELECT * FROM ReviewFood WHERE FoodMenu_ID = $1",
+        [id],
+        (error, results) => {
+            if (error) {
+                logger.error(error)
+            } else {
+                // Response and Logging
+                response.status(200).json(results.rows)
+                logger.info("GET /" + request.get("host") + request.originalUrl)
+            }
+        }
+    )
+}
+
+// -----------------------------------------------------------------------------
+//                      Share Food Table
+// -----------------------------------------------------------------------------
+
+// Create Share Food
+const createShare = (request, response) => {
+    const { ShareDate, UserAccountID, FoodMenuID } = request.body
+    pool.query(
+        "INSERT INTO ShareFood (Share_Date, UserAccount_ID, FoodMenu_ID) VALUES ($1, $2, $3)",
+        [ShareDate, UserAccountID, FoodMenuID],
+        (error, results) => {
+            if (error) {
+                logger.error(error)
+            } else {
+                // Response and Logging
+                response
+                    .status(201)
+                    .send(`Share food of user ID : ${UserAccountID}`)
+                logger.info(
+                    "POST /" + request.get("host") + request.originalUrl
+                )
+            }
+        }
+    )
+}
+
+// View All User's Share Food
+const getSharese = (request, response) => {
+    const id = parseInt(request.params.id)
+    pool.query(
+        "SELECT * FROM ShareFood AS sf JOIN FoodMenu AS fm ON sf.FoodMenu_ID = fm.ID WHERE sf.UserAccount_ID = $1",
+        [id],
+        (error, results) => {
+            if (error) {
+                logger.error(error)
+            } else {
+                // Response and Logging
+                response.status(200).json(results.rows)
+                logger.info("GET /" + request.get("host") + request.originalUrl)
             }
         }
     )
@@ -150,4 +266,10 @@ module.exports = {
     deleteUser,
     getFoods,
     createFood,
+    updateFood,
+    deleteFood,
+    createReview,
+    getReviews,
+    createShare,
+    getSharese,
 }
